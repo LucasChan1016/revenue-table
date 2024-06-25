3a382d575ba88315774b2114d8ffc9d0454051af
LucasChan1016 <chanconan@gmail.com>
2024-06-25 14:15:34 +0800

    update files

--- report.py
+++ report.py
@@ -1,13 +1,13 @@
 class Report:
     extractors = {}
-    
+
     def __init__(self, pdf_path: str) -> None:
         self.pdf_path = pdf_path
-    
+
     @classmethod
     def register(cls, name, extractor):
         cls.extractors[name] = extractor
-    
+
     def extract(self, name):
         if name not in self.extractors:
             raise ValueError(f"Unknown extractor: {name}")
